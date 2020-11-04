32661d21a4bebe1c0967f674967c7344d6bcb74e
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-11-04 23:48:13 +0700

    test

--- builder/image-validate.sh
+++ builder/image-validate.sh
@@ -5,6 +5,6 @@ set -e
 cd /home/pi/rpi_satellite_receiver/builder/tests/
 ./test.sh
 cd /home/pi
-echo "Move /etc/ld.so.preload back to its original position"
-mv /etc/ld.so.preload.disabled-for-build /etc/ld.so.preload
+# echo "Move /etc/ld.so.preload back to its original position"
+# mv /etc/ld.so.preload.disabled-for-build /etc/ld.so.preload
 
